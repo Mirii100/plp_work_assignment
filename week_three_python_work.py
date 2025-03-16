af4bbf47d21bc4590f86804b9cd0f7e3159ed600
Mirii Alex Njuguna <145835776+Mirii100@users.noreply.github.com>
2025-03-16 20:37:20 +0300

    week_three_python_work.py
    Added a condition to check valid input

--- week_three_python_work.py
+++ week_three_python_work.py
@@ -5,14 +5,17 @@ def calculate_discount(price, discount):
     Otherwise, the original price is returned.
     """
 
-  if num.isdigit():
+  if num.isdigit():# check wheteher the number entered is an interger or a string 
        
-    if discount >= 20:
+    if discount >= 20:# checks whether diiscount is greater or equal to 20
         discount_amount = (discount / 100) * price
         return price - discount_amount
 
-    else :
-      print('the input is  not a  number ')
+ else :
+      print('the input is  not a  number  enter a valid number ')
+     price = float(input("Enter the original price:   "))
+     discount_percent = float(input("Enter the discount percentage:  do not indicate the % sign    " ))
+     
     return price
 
 # Prompt user for input
