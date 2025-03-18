9890c578b56c750c00986d4d7817ad2816e15430
alex <njugunamirii99@gmail.com>
2025-03-18 10:46:07 +0300

    added python session 1 in the plp class sessiion

---
+++ tupple_week/tupple.py
@@ -0,0 +1,8 @@
+animals=('dog','cattle','sheep','chicken','pigs ')
+
+print(animals)# prints  the tupple of animals 
+print('printing the tupple in the first index  ',animals[0])
+# creates a dictionary 
+my_dictionary={'name':'Alex ','age':10,'gender':'Male','nationality':'kenyan'}
+print(my_dictionary)
+print('the element in the dictionary',my_dictionary['name'])
