5a24787d2c040458f83f9c6087690bbf63fdbfb3
alex <njugunamirii99@gmail.com>
2025-03-25 11:25:09 +0300

    week 3 loops

---
+++ week3_python/if_.py
@@ -0,0 +1,22 @@
+Grade=float (input("Enter your grade: "))
+if Grade>=90:
+        print("A")
+elif Grade>=80:
+        print("B")
+elif Grade>=70:
+        print("C")
+elif Grade>=60:
+        print("D")
+else:
+        print("F")
+
+
+
+temperature = 20
+
+if temperature >= 20:
+    print("It's warm. Wear shorts.")
+elif temperature < 20:
+    print("It's cold. Wear a jacket.")
+else:
+    print("It's perfect. Wear whatever you want.")
